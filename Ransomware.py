6eed90e74a2033264785ef44efd8724a78cfc878
T-Mees <t.mees@student.helmo.be>
2024-04-11 12:23:04 +0200

    fct get_data_from_db

---
+++ Ransomware.py
@@ -0,0 +1,49 @@
+print("""
+# ####################################################################################################################################################
+# #       ***** ***                                                                      ***** *    **   ***                                       # #
+# #    ******  * **                                                                   ******  *  *****    ***                                      # #
+# #   **   *  *  **                                                                  **   *  *     *****   ***                                     # # 
+# #  *    *  *   **                                                                 *    *  **     * **      **                                    # # 
+# #      *  *    *                              ****       ****                         *  ***     *         **            ***  ****               # # 
+# #     ** **   *       ****    ***  ****      * **** *   * ***  * *** **** ****       **   **     *         **    ****     **** **** *    ***     # # 
+# #     ** **  *       * ***  *  **** **** *  **  ****   *   ****   *** **** ***  *    **   **     *         **   * ***  *   **   ****    * ***    # #
+# #     ** ****       *   ****    **   ****  ****       **    **     **  **** ****     **   **     *         **  *   ****    **          *   ***   # #
+# #     ** **  ***   **    **     **    **     ***      **    **     **   **   **      **   **     *         ** **    **     **         **    ***  # #
+# #     ** **    **  **    **     **    **       ***    **    **     **   **   **      **   **     *         ** **    **     **         ********   # #
+# #     *  **    **  **    **     **    **    ****  **  **    **     **   **   **       **  **     *         ** **    **     **         *******    # #
+# #        *     **  **    **     **    **   * **** *    ******      **   **   **        ** *      *         *  **    **     **         **         # #
+# #    ****      *** **    **     **    **     ****      ****       ***  ***  ***         ***      ***      *   **    **     ***        ****    *  # #
+# #   *  ****    **   ***** **    ***   ***                          ***  ***  ***          ******** ********     ***** **     ***        *******  # #
+# #  *    **     *     ***   **    ***   ***                          ***  ***  ***          ****     ****        ***   **                *****    # #
+# #  *                                                                                                                                             # #
+# #   **                                                                                                                                           # #
+# #                                                                                                                      By_Les_Gargouilles 🗿🗿🗿# #
+# ################################################################################################################################################## #
+#                                                                                                                                                    #
+# YOUR IMPORTANT FILES ARE ENCRYPTED                                                                                                                 #
+# ==================================                                                                                                                 #
+#                                                                                                                                                    #
+# Many of your documents, photos, images and other files are no longer accessible because                                                            #
+# they have been encrypted with military grad encryption algorithm.                                                                                  #
+# Maybe you are busy looking for a way to recover you files, but do not waste your time.                                                             # 
+# Nobody can recover your files without our decryption service.                                                                                      #
+#                                                                                                                                                    #
+#                                                                                                                                                    #
+# Anyways, we guarantee that you can recover your files safely and easily. This will require us to use some processing power,                        #
+# electricity and storage on our side, to there's a fixed processing fee of 150 USD. This is a one-time payment, no additional fees included.        #
+# In order to accept this offer, you have to deposit payment within 96 hours (4 days) after receiving this message,                                  #
+# otherwise this offer will expire and you will lose your files forever.                                                                             #
+#                                                                                                                                                    #
+# Payment has to be deposited in Bitcoin based on Bitcoin/USD exchange rate at the moment of payement. The address you have to make payement is:     #
+#                                                                                                                                                    #
+#     43viFPsZf7pR3ZoKeva9yoQjrexQiDNNX9                                                                                                             #
+#     (This is a fake address)                                                                                                                       #
+#                                                                                                                                                    #
+# Copy the fake address and press enter to simulate the ransom payment :                                                                             #
+# 43viFPsZf7pR3ZoKeva9yoQjrexQiDNNX9                                                                                                                 #
+#                                                                                                                                                    # 
+# Decryption will start automatically within 48 hours after the payment has been processe. After that all of your files will be restored.            #
+# THIS OFFER IS VALIDE FOR 96 HOURS AFTER RECEIVING THIS MESSAGE                                                                                     #                                                
+#                                                                                                                                                    #
+######################################################################################################################################################
+""")
